c72aa6f0673e43939bce11393d4ff43d8ee2b091
yntymakdev <kubanychevyntymak21@gmail.com>
2025-07-13 12:57:00 +0600

    sa

--- src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
+++ src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
@@ -1,4 +1,18 @@
 package com.dailycodework.dreamshops.service.product;
 
+import com.dailycodework.dreamshops.model.Product;
+import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
+
+import java.util.List;
+
 public interface IProductService {
+    Product addProduct(Product product);
+    Product  getProductById(Long id);
+    void  deleteProductById(Long id);
+    void updateProduct(Product product,Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand   );
+    List<Product> getProductsByCategoryAndBrand(String category,String brand);
+
 }
